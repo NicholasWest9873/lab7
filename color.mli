adc8ee43b7e786b9157100b2073d040ce1683332
Nicholas West <nicholaswest@college.harvard.edu>
2020-03-01 22:28:45 -0500

    done

---
+++ color.mli
@@ -0,0 +1,11 @@
+(*
+                         CS 51 Lab 7
+                           Abstract Data Types
+
+                          Color Interface
+*)
+
+type color = int
+type color_name = | Red | Green | Blue| Orange | Yellow | Indigo | Violet
+
+val to_color :
